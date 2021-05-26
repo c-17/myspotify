5b1f9dc03c80a875e5f7b45c2c9a21f9fc26bfc9
ChristianKing17 <cccardona17@gmail.com>
2021-05-26 17:28:40 -0500

    Limpiando Código...

--- App/Views/Dashboard.Designer.cs
+++ App/Views/Dashboard.Designer.cs
@@ -39,8 +39,6 @@ namespace MySpotify.Views
             this.label3 = new System.Windows.Forms.Label();
             this.TextBoxSearcher = new System.Windows.Forms.TextBox();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.ButtonExplore = new System.Windows.Forms.Button();
@@ -112,7 +110,7 @@ namespace MySpotify.Views
             this.LabelUser.Name = "LabelUser";
             this.LabelUser.Size = new System.Drawing.Size(169, 26);
             this.LabelUser.TabIndex = 0;
-            this.LabelUser.Text = "label4";
+            this.LabelUser.Text = "User";
             this.LabelUser.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // button6
@@ -132,6 +130,7 @@ namespace MySpotify.Views
             this.button6.TabIndex = 7;
             this.button6.Text = "Songs";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Visible = false;
             // 
             // button5
             // 
@@ -150,6 +149,7 @@ namespace MySpotify.Views
             this.button5.TabIndex = 6;
             this.button5.Text = "Albums";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Visible = false;
             // 
             // button4
             // 
@@ -168,6 +168,7 @@ namespace MySpotify.Views
             this.button4.TabIndex = 5;
             this.button4.Text = "Artists";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
             // 
             // label3
             // 
@@ -181,6 +182,7 @@ namespace MySpotify.Views
             this.label3.TabIndex = 4;
             this.label3.Text = "Favorites";
             this.label3.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.label3.Visible = false;
             // 
             // TextBoxSearcher
             // 
@@ -195,13 +197,11 @@ namespace MySpotify.Views
             // 
             // tableLayoutPanel2
             // 
-            this.tableLayoutPanel2.ColumnCount = 4;
+            this.tableLayoutPanel2.ColumnCount = 2;
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 83F));
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 32F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 32F));
-            this.tableLayoutPanel2.Controls.Add(this.button1, 2, 0);
-            this.tableLayoutPanel2.Controls.Add(this.button2, 3, 0);
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel2.Controls.Add(this.label2, 0, 0);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
@@ -211,36 +211,6 @@ namespace MySpotify.Views
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel2.Size = new System.Drawing.Size(175, 32);
             this.tableLayoutPanel2.TabIndex = 3;
-            // 
-            // button1
-            // 
-            this.button1.BackgroundImage = global::MySpotify.Properties.Resources.ic_navigate_back;
-            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(113, 2);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(26, 26);
-            this.button1.TabIndex = 1;
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Visible = false;
-            // 
-            // button2
-            // 
-            this.button2.BackgroundImage = global::MySpotify.Properties.Resources.ic_navigate_next;
-            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button2.FlatAppearance.BorderSize = 0;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Location = new System.Drawing.Point(145, 2);
-            this.button2.Margin = new System.Windows.Forms.Padding(2);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(26, 26);
-            this.button2.TabIndex = 1;
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Visible = false;
             // 
             // label2
             // 
@@ -341,9 +311,7 @@ namespace MySpotify.Views
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox TextBoxSearcher;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
         private System.Windows.Forms.Button ButtonExplore;
         private System.Windows.Forms.Label label1;
